49bb5872518da99d93d194cae2a2cbcba0f6d773
Phinner <62483793+Phinner@users.noreply.github.com>
2022-03-10 18:11:19 +0100

    v2.4.0 release II

--- build-logic/src/main/kotlin/distributor.parent-build-logic.gradle.kts
+++ build-logic/src/main/kotlin/distributor.parent-build-logic.gradle.kts
@@ -1,17 +1,3 @@
 plugins {
     id("net.kyori.indra.git")
 }
-
-tasks.create("createRelease") {
-    dependsOn("requireClean")
-
-    doLast {
-        exec {
-            commandLine("git", "tag", "-as", "v${project.version}", "-F", "./CHANGELOG.md")
-        }
-
-        exec {
-            commandLine("git", "push", "origin", "--tags")
-        }
-    }
-}
--- build.gradle.kts
+++ build.gradle.kts
@@ -7,3 +7,17 @@ val metadata = readJson(file("global-plugin.json"))
 group = "fr.xpdustry"
 version = (metadata["version"] as String) + if (indraGit.headTag() == null) "-SNAPSHOT" else ""
 description = "The Mindustry plugin of ur dreams..."
+
+tasks.create("createRelease") {
+    dependsOn("requireClean")
+
+    doLast {
+        exec {
+            commandLine("git", "tag", "-as", "v${metadata["version"]}", "-F", "./CHANGELOG.md")
+        }
+
+        exec {
+            commandLine("git", "push", "origin", "--tags")
+        }
+    }
+}
