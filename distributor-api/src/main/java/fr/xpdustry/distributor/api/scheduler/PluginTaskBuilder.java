1bc4a40bb2e33dba2309a08e22e474ade44562c1
phinner <62483793+Phinner@users.noreply.github.com>
2023-03-21 15:39:55 +0100

    chore: Update license to 2023

--- distributor-api/src/main/java/fr/xpdustry/distributor/api/scheduler/PluginTaskBuilder.java
+++ distributor-api/src/main/java/fr/xpdustry/distributor/api/scheduler/PluginTaskBuilder.java
@@ -1,7 +1,7 @@
 /*
  * Distributor, a feature-rich framework for Mindustry plugins.
  *
- * Copyright (C) 2022 Xpdustry
+ * Copyright (C) 2023 Xpdustry
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
