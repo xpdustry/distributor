cbb382462b07fc3c8468a0dc1a4bd61502d1dd08
phinner <62483793+Phinner@users.noreply.github.com>
2023-04-12 14:51:28 +0200

    chore: Bump Toxopid version

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 dependencies {
     implementation("net.kyori:indra-common:3.0.1")
     implementation("net.kyori:indra-licenser-spotless:3.0.1")
-    implementation("fr.xpdustry:toxopid:3.0.0")
+    implementation("fr.xpdustry:toxopid:3.1.0")
     implementation("gradle.plugin.com.github.johnrengelman:shadow:7.1.2")
     implementation("net.ltgt.gradle:gradle-errorprone-plugin:2.0.2")
     implementation("com.diffplug.spotless:spotless-plugin-gradle:6.11.0")
