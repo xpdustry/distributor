78cbd5ff1414d85b54c8db5e0ee10c42c99c9d54
phinner <62483793+phinner@users.noreply.github.com>
2023-12-08 04:11:45 +0100

    chore: Bump distributor version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,6 @@ plugins {
     id("distributor.parent-conventions")
 }
 
-version = "3.2.1" + if (indraGit.headTag() == null) "-SNAPSHOT" else ""
+version = "3.2.2" + if (indraGit.headTag() == null) "-SNAPSHOT" else ""
 group = "fr.xpdustry"
 description = "The Mindustry plugin of ur dreams..."
