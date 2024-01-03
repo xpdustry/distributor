a37badb53342e0f68b253688799d8e544eb7db96
phinner <62483793+phinner@users.noreply.github.com>
2024-01-03 19:15:22 +0100

    chore: Prepare next version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,6 @@ plugins {
     id("distributor.parent-conventions")
 }
 
-version = "3.3.0" + if (indraGit.headTag() == null) "-SNAPSHOT" else ""
+version = "3.3.1" + if (indraGit.headTag() == null) "-SNAPSHOT" else ""
 group = "fr.xpdustry"
 description = "The Mindustry plugin of ur dreams..."
