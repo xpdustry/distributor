34a7af36a5a98a06423a9f661376d39a02d7bc94
phinner <62483793+Phinner@users.noreply.github.com>
2024-03-07 10:44:04 +0100

    feat(build): Added gradle versions plugin + dependency bumps

--- distributor-build-logic/build.gradle.kts
+++ distributor-build-logic/build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
     implementation(libs.bundles.indra)
     implementation(libs.errorprone.gradle)
     implementation(libs.mammoth)
+    implementation(libs.gradle.versions)
 
     // https://github.com/gradle/gradle/issues/15383#issuecomment-779893192
     implementation(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
