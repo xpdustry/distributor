bd3fa908532ab3f347a3c6bfdfe980d9f54c70e3
phinner <62483793+phinner@users.noreply.github.com>
2024-03-09 14:47:33 +0100

    chore(build): Explicit types for distributor module extension

--- distributor-build-logic/src/main/kotlin/extensions.kt
+++ distributor-build-logic/src/main/kotlin/extensions.kt
@@ -2,6 +2,8 @@ import net.kyori.mammoth.Extensions
 import org.gradle.accessors.dm.LibrariesForLibs
 import org.gradle.api.Project
 import org.gradle.api.plugins.ExtensionContainer
+import org.gradle.api.provider.Property
+import org.gradle.api.provider.SetProperty
 import org.gradle.kotlin.dsl.the
 
 internal val Project.libs: LibrariesForLibs get() = the()
@@ -9,11 +11,11 @@ internal val Project.libs: LibrariesForLibs get() = the()
 inline fun <reified T : Any> ExtensionContainer.findOrCreateExtension(name: String): T = Extensions.findOrCreate(this, name, T::class.java)
 
 open class DistributorModuleExtension(project: Project) {
-    val identifier = project.objects.property(String::class.java)
-    val display = project.objects.property(String::class.java)
-    val main = project.objects.property(String::class.java)
-    val description = project.objects.property(String::class.java)
+    val identifier: Property<String> = project.objects.property(String::class.java)
+    val display: Property<String> = project.objects.property(String::class.java)
+    val main: Property<String> = project.objects.property(String::class.java)
+    val description: Property<String> = project.objects.property(String::class.java)
 
     // TODO Does not handle transitive dependencies, FIX IT
-    val dependencies = project.objects.setProperty(Project::class.java)
+    val dependencies: SetProperty<Project> = project.objects.setProperty(Project::class.java)
 }
