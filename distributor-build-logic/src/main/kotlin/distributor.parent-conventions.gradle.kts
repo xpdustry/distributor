f575d4c9f5cc9600da7a463a34a1d16446c3307b
phinner <62483793+phinner@users.noreply.github.com>
2024-04-03 17:20:28 +0200

    chore: Remove `com.github.ben-manes.versions`

--- distributor-build-logic/src/main/kotlin/distributor.parent-conventions.gradle.kts
+++ distributor-build-logic/src/main/kotlin/distributor.parent-conventions.gradle.kts
@@ -5,7 +5,6 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 plugins {
     id("com.diffplug.spotless")
     id("net.kyori.indra.git")
-    id("com.github.ben-manes.versions")
 }
 
 repositories {
@@ -20,21 +19,6 @@ extensions.configure<SpotlessExtensionPredeclare> {
     java { palantirJavaFormat(libs.versions.palantir.get()) }
     kotlin { ktlint(libs.versions.ktlint.get()) }
     kotlinGradle { ktlint(libs.versions.ktlint.get()) }
-}
-
-// https://github.com/ben-manes/gradle-versions-plugin
-fun isNonStable(version: String): Boolean {
-    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
-    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
-    val isStable = stableKeyword || regex.matches(version)
-    return isStable.not()
-}
-
-tasks.dependencyUpdates {
-    // https://github.com/ben-manes/gradle-versions-plugin
-    rejectVersionIf {
-        isNonStable(candidate.version) && !isNonStable(currentVersion)
-    }
 }
 
 tasks.register<Copy>("release") {
